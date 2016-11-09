8892e39f83e8b86360fac9f20c2f75bf5f9b228d
GalRavil <gal.ravil93@gmail.com>
2016-11-09 21:10:25 +0300

    +- Post, +PublishedManager

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -19,6 +25,9 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    objects = models.Manager()
+    published = PublishedManager()
+    
     class Meta:
         ordering = ('-publish',)
 
